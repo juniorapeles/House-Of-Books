534b2d1c80cf29c5bee08962027d6212c36a1fec
eli <elijunior.apeles2005@gmail.com>
2024-02-18 17:52:24 -0300

    feat - delete book From FrontEnd

--- backend/src/main/java/com/techlibrary/houseofbooks/repositories/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.techlibrary.houseofbooks.repositories;
-
-import com.techlibrary.houseofbooks.entities.Loan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-}
