2a82422761f219d32d931493b9963158f959f860
eli <elijunior.apeles2005@gmail.com>
2025-12-17 14:56:19 -0300

    feat: subindo para prod, vamo ver no que dar!! hahahahaaha

--- src/main/java/com/techlibrary/generics/GenericServiceImpl.java
+++ src/main/java/com/techlibrary/generics/GenericServiceImpl.java
@@ -4,13 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public abstract class GenericServiceImpl<E, DTO, ID, M extends GenericMapper<E, DTO>>
+public abstract class GenericServiceImpl<ENTITY, DTO, ID, MAPPER extends GenericMapper<ENTITY, DTO>>
         implements GenericService<DTO, ID> {
 
-    protected final JpaRepository<E, ID> repository;
-    protected final M mapper;
+    protected final JpaRepository<ENTITY, ID> repository;
+    protected final MAPPER mapper;
 
-    protected GenericServiceImpl(JpaRepository<E, ID> repository, M mapper) {
+    protected GenericServiceImpl(JpaRepository<ENTITY, ID> repository, MAPPER mapper) {
         this.repository = repository;
         this.mapper = mapper;
     }
@@ -24,25 +24,25 @@ public abstract class GenericServiceImpl<E, DTO, ID, M extends GenericMapper<E, 
 
     @Override
     public DTO getById(ID id) {
-        E entity = repository.findById(id)
+        ENTITY entity = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Entidade não encontrada"));
         return mapper.toDTO(entity);
     }
 
     @Override
     public DTO create(DTO dto) {
-        E entity = mapper.toEntity(dto);
-        E saved = repository.save(entity);
+        ENTITY entity = mapper.toEntity(dto);
+        ENTITY saved = repository.save(entity);
         return mapper.toDTO(saved);
     }
 
     @Override
     public DTO update(ID id, DTO dto) {
-        E entity = repository.findById(id)
+        ENTITY entity = repository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Entidade não encontrada"));
 
         mapper.updateEntityFromDTO(dto, entity);
-        E updated = repository.save(entity);
+        ENTITY updated = repository.save(entity);
         return mapper.toDTO(updated);
     }
 
